66a00aa69aa5b024b97cc3a7d5027a59aec20b09
Bunsal.Hul <bunsal.hul@student.passerellesnumeriques.org>
2021-12-18 15:50:52 +0700

    #1 Create sign in form

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import SignIn_SignUp from '../components/views/SignIn_SignUp.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  
+  {
+    path: '/',
+    name: 'signin',
+    component: SignIn_SignUp
+  },
 ]
 
 const router = new VueRouter({
