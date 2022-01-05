bdb0b0940f24376481e6b38c7ba4156363d29803
Bunsal.Hul <bunsal.hul@student.passerellesnumeriques.org>
2022-01-05 19:32:03 +0700

    #0 final commit on develop

--- front/src/components/views/Event.vue
+++ front/src/components/views/Event.vue
@@ -1,3 +1,8 @@
 <template>
-    <h2>event</h2>
+    <div class="mt-5 blue--text" align="center">
+        <h2>
+            Event is coming soon !
+        </h2>
+        <img src="https://thumbs.dreamstime.com/b/white-gift-blue-ribbon-bow-isolated-background-37035641.jpg" alt="" style="width: 50%">
+    </div>
 </template>
